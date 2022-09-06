6ef03b94f31eff9b34ddffa8b802c85e85f240d7
Chisen Kaoru <cordx56@cordx.net>
2022-09-07 07:28:47 +0900

    update packaging

--- src/index.ts
+++ src/index.ts
@@ -12,18 +12,3 @@ export {
   transform,
 } from "./tools";
 export { sequence, next } from "./sequence";
-export {
-  bytes,
-  until,
-  space0,
-  space1,
-  multiSpace0,
-  multiSpace1,
-  alpha0,
-  alpha1,
-  numeric0,
-  numeric1,
-  alphaNumeric0,
-  alphaNumeric1,
-} from "./bytes";
-export { integer, float } from "./number";
