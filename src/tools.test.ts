c4bd74c6f99902f79fc08dc7ca56ed26cf613ed0
Chisen Kaoru <cordx56@cordx.net>
2022-09-07 01:04:46 +0900

    add tools test

--- src/tools.test.ts
+++ src/tools.test.ts
@@ -1,6 +1,7 @@
 import { ParseSuccess } from "./jsdrink";
 import { ParseError } from "./error";
 import { alpha1, bytes, numeric1 } from "./bytes";
+import { integer } from "./number";
 import {
   inputToBytes,
   optional,
@@ -23,5 +24,77 @@ describe("optional parser", () => {
     expect(optional(bytes("abc"))("123abc")).toStrictEqual(
       new ParseSuccess(null, "123abc")
     );
+  });
+});
+
+describe("any parser", () => {
+  test("any success", () => {
+    expect(any(bytes("abc"), bytes("123"))("123abc")).toStrictEqual(
+      new ParseSuccess(inputToBytes("123"), inputToBytes("abc"))
+    );
+  });
+  test("any failure", () => {
+    checkError(
+      any(bytes("abc"), bytes("123"))("test"),
+      new ParseError("any", 4, null)
+    );
+  });
+});
+
+describe("transform parsed result", () => {
+  test("add 1 to parsed integer", () => {
+    expect(
+      transform(integer, (a) => (a !== null ? a + 1 : a))("123")
+    ).toStrictEqual(new ParseSuccess(124, inputToBytes("")));
+  });
+});
+
+describe("many0 parser", () => {
+  test("two matched bytes", () => {
+    expect(many0(bytes("abc"))("abcabc")).toStrictEqual(
+      new ParseSuccess(
+        [inputToBytes("abc"), inputToBytes("abc")],
+        inputToBytes("")
+      )
+    );
+  });
+  test("no matched bytes", () => {
+    expect(many0(bytes("abc"))("defghi")).toStrictEqual(
+      new ParseSuccess([], "defghi")
+    );
+  });
+});
+
+describe("many1 parser", () => {
+  test("two matched bytes", () => {
+    expect(many1(bytes("abc"))("abcabc")).toStrictEqual(
+      new ParseSuccess(
+        [inputToBytes("abc"), inputToBytes("abc")],
+        inputToBytes("")
+      )
+    );
+  });
+  test("no matched bytes error", () => {
+    checkError(
+      many1(bytes("abc"))("defghi"),
+      new ParseError("many1", 6, new ParseError("bytes", 6, null))
+    );
+  });
+});
+
+describe("noRemain parser", () => {
+  test("remain error", () => {
+    checkError(noRemain(alpha1)("abc123"), new ParseError("noRemain", 3, null));
+  });
+});
+
+describe("not parser", () => {
+  test("not parser success", () => {
+    expect(not(alpha1)("123abc")).toStrictEqual(
+      new ParseSuccess("123abc", inputToBytes(""))
+    );
+  });
+  test("not parser error", () => {
+    checkError(not(alpha1)("abc123"), new ParseError("not", 6, null));
   });
 });
